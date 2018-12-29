5f07967099ed68e47584348e9f2bf406d832d8ce
Andre Lara <andre.amorim.si@gmail.com>
2018-12-29 11:48:37 -0200

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Injector } from '@angular/core';
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import { Observable } from 'rxjs';
+import { map, flatMap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,42 @@ export class EntryService extends BaseResourceService<Entry> {
   constructor(protected injector: Injector, private categoryService: CategoryService) {
     super('api/entries', injector, Entry.fromJson);
   }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  // getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+  //   return this.getAll().pipe(
+  //     map(entries => this.filterByMonthAndYear(entries, month, year))
+  //   );
+  // }
+
+  // PRIVATE METHODS
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  // private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+  //   return entries.filter(entry => {
+  //     const entryDate = moment(entry.date, "DD/MM/YYYY");
+  //     const monthMatches = entryDate.month() + 1 === month;
+  //     const yearMatches = entryDate.year() === year;
+
+  //     if(monthMatches && yearMatches) return entry;
+  //   });
+  // }
+
+
 }
